c5ac18362e2111015fe8dce77a0133aac5e7299c
HaibiPeng <haibi.peng@aliyun.com>
2022-10-30 01:29:22 +0800

    latest version

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,41 +1,49 @@
 // index.ts
 // 获取应用实例
-import { isNum } from '../../utils/util';
+import { isPercentage, isGpa, isPercentageAndGpa, getGrade, SUBJECTS, REGIONS, schools } from '../../utils/util';
 import Message from '../../miniprogram_npm/tdesign-miniprogram/message/index';
 
-const app = getApp<IAppOption>()
+const app = getApp<IAppOption>();
+
 const PICKER_KEY = {
-    SCHOOL: 'school'
+    SCHOOL: 'school',
+    SUBJECT: 'subject'
 };
+const REGION_LABELS = Object.keys(REGIONS).map((key) => {
+    return { label: key, value: (REGIONS as any)[key] }
+});
 
 Component({
     data: {
         schoolValue: '',
-        gpaValue: '',
+        subjectValue: '',
+        gradeValue: '',
         destinationsValue: [],
+        formValue: {},
         PICKER_KEY,
         [`${PICKER_KEY.SCHOOL}Visible`]: false,
+        [`${PICKER_KEY.SUBJECT}Visible`]: false,
         pickerTitle: '',
         schools: [
-            { label: '清北', value: '清北' },
-            { label: 'C9', value: 'C9' },
-            { label: '普通985', value: '985' },
-            { label: '211', value: '211' },
-            { label: '普通一本', value: '普通一本' },
+            { label: '985/211', value: '985/211' },
+            { label: '双非', value: '双非' },
+            { label: '二本', value: '二本' },
             { label: '海本', value: '海本' },
             { label: '其他', value: '其他' },
         ],
+        subjects: Object.keys(SUBJECTS).map((key: string) => {
+            return { label: key, value: key }
+        }),
         destinations: [
             { label: '全选', checkAll: true },
-            { label: '北美', value: '北美' },
-            { label: '英国', value: '英国' },
-            { label: '澳洲', value: '澳洲' },
-            { label: '香港/新加坡', value: '香港/新加坡' },
-            { label: '北欧', value: '北欧' },
-            { label: '其他', value: '其他' },
+            ...REGION_LABELS
         ],
         [`${PICKER_KEY.SCHOOL}Value`]: [],
-        checkAllValues: []
+        [`${PICKER_KEY.SUBJECT}Value`]: [],
+        checkAllValues: [],
+        visible: false
+        // checkResultsDisabled: true,
+        // checkResultIcon: 'close-circle'
     },
     
     methods: {
@@ -48,12 +56,12 @@ Component({
                 [`${key}Visible`]: true,
             });
         },
-        onColumnChange(e: any) {
-            console.log('picker pick:', e);
+        onColumnChange(_e: any) {
+            // console.log('picker pick:', e);
         },
         onPickerChange(e: any) {
             const { key } = e?.currentTarget?.dataset;
-            console.log('picker change:', e.detail);
+            // console.log('picker change:', e.detail);
             this.setData({
                 [`${key}Visible`]: false,
                 [`${key}Value`]: e.detail.value,
@@ -71,7 +79,7 @@ Component({
         onInput(e: any) {
             // console.log(e.detail.value);
             this.setData({
-                gpaValue: e.detail.value
+                gradeValue: e.detail.value
             });
         },
         onCheckAllChange(e: any) {
@@ -82,42 +90,70 @@ Component({
             });
         },
         onFormSubmit() {
-            // console.log(this.data.schoolValue);
-            // console.log(this.data.gpaValue);
-            // console.log(this.data.destinationsValue);
             let ready = this.formValidation();
             if (ready) {
-                console.log('send data');
+                // this.setData({
+                //     checkResultsDisabled: false,
+                //     checkResultIcon: 'check-circle'
+                // });
+                // console.log('send data');
+                this.setData({
+                  visible: true
+                });
+                this.postRequest();
+                // wx.setStorageSync('data', schools);
+                // setTimeout(() => {
+                //     wx.navigateTo({
+                //         url: '../results/results'
+                //     });
+                //     this.setData({
+                //         visible: false
+                //     });
+                // }, 1000);
             } else {
                 console.log('oops! sth went wrong...');
             }
         },
         formValidation() {
             let school = this.data.schoolValue;
-            let gpa = this.data.gpaValue;
+            let subject = this.data.subjectValue;
+            let grade = this.data.gradeValue;
             let destinations = this.data.destinationsValue;
             let schoolValid = school.length === 1;
-            let gpaValid = isNum(gpa) && parseInt(gpa) <= 100;
+            let subjectValid = subject.length === 1;
+            let { type, gradeValid } = this.isGradeValid(grade);
             let destinationValid = destinations.length !== 0;
             // console.log('schoolValid:', schoolValid);
-            // console.log('gpaValid:', gpaValid);
+            // console.log('gradeValid:', gradeValid);
             // console.log('destinationValid:', destinationValid);
-            let isValid = schoolValid && gpaValid && destinationValid;
+            let isValid = schoolValid && subjectValid && gradeValid && destinationValid;
             if (isValid) {
+            // if (true) {
                 this.showSuccessMessage();
+                const grade = getGrade(type, this.data.gradeValue);
+                console.log(grade);
+                this.setData({
+                    formValue: {
+                        school_name: school[0],
+                        subject: SUBJECTS[subject[0]],
+                        grade: grade,
+                        destination_region: destinations
+                    }
+                });
+                // wx.setStorageSync('grade', { percentage_score: 85, gpa_score: 3.76 });
+                wx.setStorageSync('grade', grade);
                 return true;
             } else {
                 this.showErrorMessage();
                 return false;
             }
         },
-
         showSuccessMessage() {
             Message.success({
               context: this,
               offset: [20, 32],
               duration: 3000,
-              content: '提交成功',
+              content: '提交成功！请等待跳转至结果页面',
             });
         },
         showErrorMessage() {
@@ -128,5 +164,65 @@ Component({
                 content: '请按要求选择填写对应信息',
             });
         },
+        isGradeValid(grade: string) {
+            let type = 'wrong';
+            let gradeValid = false;
+            if (isPercentage(grade)) {
+                type = 'percentage';
+                gradeValid = true;
+            } else if (!isPercentage(grade) && isGpa(grade)) {
+                type = 'gpa'
+                gradeValid = true;
+            } else if (!isPercentage(grade) && !isGpa(grade) &&isPercentageAndGpa(grade)){
+                type = 'both';
+                gradeValid = true;
+            }
+            console.log(type);
+            return { type, gradeValid };
+        },
+        bindResultTap() {
+          wx.navigateTo({
+            url: '../results/results',
+          });
+        },
+        onShow: function() {
+          console.log('loaded');
+          wx.setStorageSync('data', []);
+        },
+        onVisibleChange(e: any) {
+            this.setData({
+                visible: e.detail.visible,
+            });
+        },
+        postRequest() {
+            // console.log("form data:", this.data.formValue);
+            let that = this;
+            wx.request({
+                url: 'http://120.79.13.214:8080/get_filter_school',
+                data: this.data.formValue,
+                header: {
+                    "Content-Type": "application/json"
+                    //"Content-Type": "application/x-www-form-urlencoded"
+                },
+                method: 'POST',
+                dataType:'json',
+                success: function (res: any) {
+                    const response = res.data;
+                    // console.log(response['msg']);
+                    const results = JSON.parse(response['result']).apply_results;         
+                    wx.setStorageSync('data', results)
+                    wx.navigateTo({
+                        url: '../results/results'
+                    });
+                    that.setData({
+                        visible: false
+                    });
+                },
+                fail: function (err: any) {
+                    console.log(err)
+                },
+                complete: function () { }//请求完成后执行的函数
+            });
+        }
     },
 });
--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -13,9 +13,297 @@ export const formatTime = (date: Date) => {
   )
 }
 
-export const isNum = (value: any) => !isNaN(parseFloat(value)) && isFinite(value);
+export const isNumeric = (value: any) => {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+export const isPercentage = (value: string) => {
+    return isNumeric(value) && parseFloat(value) <= 100;
+}
+
+export const isGpa = (value: string) => {
+    const [gpa, fullGPA] = value.split('/');
+    return [gpa, fullGPA].every(item => isNumeric(item)) && parseFloat(gpa) <= parseFloat(fullGPA);
+}
+
+export const isPercentageAndGpa = (value: string) => {
+    if (value.includes('+')) {
+        const [percentage, gpa] = value.split('+');
+        return parseFloat(percentage) <= 100 && isGpa(gpa);
+    } else {
+        return false;
+    }
+}
+
+export const getGrade = (type: string, grade: string) => {
+    switch (type) {
+        case 'percentage':
+            return {
+                percentage_score: parseFloat(grade),
+                gpa_score: 0
+            };
+        case 'gpa':
+            return {
+                percentage_score: 0,
+                gpa_score: parseFloat(grade.split('/')[0])
+            };
+        case 'both':
+            const [percentage, gpa] = grade.split('+');
+            return {
+                percentage_score: parseFloat(percentage),
+                gpa_score: parseFloat(gpa.split('/')[0])
+            };
+        default:
+            return {
+                percentage_score: 0,
+                gpa_score: 0
+            };
+    }
+}
 
 const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s
 }
+export const SUBJECTS: Object = {
+  "经济": 535,
+  "人文社科": 876,
+  "自然科学": 877,
+  "工程科学": 878,
+  "计算机科学": 957,
+  "商学": 969,
+  "数学统计": 970,
+  "生农医药": 971,
+  "法律": 997,
+  "设计/建筑": 1010,
+  "电子电气": 1046
+};
+
+export const REGIONS: Object = {
+    "北美": "North America",
+    "欧洲": "Europe",
+    "亚洲": "Asia",
+    "大洋洲": "Oceania",
+    "其他": "Others"
+};
+
+export const REGIONS_ZH: Object = {
+    "North America": "北美",
+    "Europe": "欧洲",
+    "Asia": "亚洲",
+    "Oceania": "大洋洲",
+    "Others": "其他"
+};
+
+export const schools = [
+    {
+      "school_name":"University of Saclay, Paris",
+      "region": "Europe",
+      "country": "法国",
+      "gpa_range":[
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "percentage_range":[
+        {"accepted_num":5,"rejected_num":4},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":7,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":6},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "total_result":
+        {"accepted_num":40,"rejected_num":9},
+      "avg_grade":
+        {"percentage_score": 85.32324324233,"PercentageNum":0,"gpa_score":3.8423413241234,"GpaNum":4},
+      "school_range":[
+        {"accepted_num":5,"rejected_num":1},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+      ]
+    },
+    {
+      "school_name":"Sorbonne University",
+      "region": "Europe",
+      "country": "法国",
+      "gpa_range":[
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "percentage_range":[
+        {"accepted_num":5,"rejected_num":4},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":7,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":6},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "total_result":
+        {"accepted_num":40,"rejected_num":9},
+      "avg_grade":
+        {"percentage_score": 85.32324324233,"PercentageNum":0,"gpa_score":3.8423413241234,"GpaNum":4},
+      "school_range":[
+        {"accepted_num":5,"rejected_num":1},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+      ]
+    },
+    {
+      "school_name":"KTH",
+      "region": "Europe",
+      "country": "瑞典",
+      "gpa_range":[
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "percentage_range":[
+        {"accepted_num":5,"rejected_num":4},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":7,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":6},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "total_result":
+        {"accepted_num":40,"rejected_num":9},
+      "avg_grade":
+        {"percentage_score": 85.3,"PercentageNum":0,"gpa_score":3.84,"GpaNum":4},
+      "school_range":[
+        {"accepted_num":5,"rejected_num":1},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+      ]
+    },
+    {
+      "school_name":"UCB",
+      "region": "North America",
+      "country": "加拿大",
+      "gpa_range":[
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "percentage_range":[
+        {"accepted_num":5,"rejected_num":4},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":7,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":6},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "total_result":
+        {"accepted_num":40,"rejected_num":9},
+      "avg_grade":
+        {"percentage_score": 85.3,"PercentageNum":0,"gpa_score":3.84,"GpaNum":4},
+      "school_range":[
+        {"accepted_num":5,"rejected_num":1},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+      ]
+    },
+    {
+      "school_name":"Harvard University",
+      "region": "North America",
+      "country": "美国",
+      "gpa_range":[
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "percentage_range":[
+        {"accepted_num":5,"rejected_num":4},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":7,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":4},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":0},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":2,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":6},
+        {"accepted_num":0,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":0}
+      ],
+      "total_result":
+        {"accepted_num":40,"rejected_num":9},
+      "avg_grade":
+        {"percentage_score": 85.3,"PercentageNum":0,"gpa_score":3.84,"GpaNum":4},
+      "school_range":[
+        {"accepted_num":5,"rejected_num":1},
+        {"accepted_num":3,"rejected_num":0},
+        {"accepted_num":0,"rejected_num":2},
+        {"accepted_num":4,"rejected_num":2},
+        {"accepted_num":0,"rejected_num":0},
+      ]
+    }
+  ];
